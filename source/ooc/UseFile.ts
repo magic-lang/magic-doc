dc92b1121a171ef88bddf3d5d8a52e36a254721f
Thomas Fanell <thomas.fanell@vidhance.com>
2016-04-18 10:40:21 +0200

    Added use-file parser

--- source/ooc/UseFile.ts
+++ source/ooc/UseFile.ts
@@ -1,12 +1,62 @@
 import * as fs from "fs";
 
 export class UseFile {
-    constructor(private name: string, private description: string, private requires: string[], private imports: string[]) {}
+    constructor(private name: string, private description: string, private sourcePath: string, private requires: string[], private imports: string[]) {}
     getName(): string { return this.name; }
     getDescription(): string { return this.description; }
+    getSourcePath(): string { return this.sourcePath; }
     getRequires(): string[] { return this.requires; }
     getImports(): string[] { return this.imports; }
+    //
+    // TODO: Refactor
+    //
     static parse(file: string): UseFile {
-        return null;
+        var result: UseFile;
+        var name: string;
+        var description: string;
+        var sourcePath: string;
+        var requires: string[] = [];
+        var imports: string[] = [];
+        var useFileLines = fs.readFileSync(file, "utf-8").split("\n");
+        for (var i = 0; i < useFileLines.length; i++) {
+            var tokenEndIndex: number;
+            if ((tokenEndIndex = useFileLines[i].indexOf(":")) > -1) {
+                var token = useFileLines[i].substring(0, tokenEndIndex).trim();
+                var value = useFileLines[i].substring(tokenEndIndex + 1).trim();
+                switch (token) {
+                    case "Name":
+                        name = value;
+                        break;
+                    case "Description":
+                        description = value;
+                        break;
+                    case "SourcePath":
+                        sourcePath = value;
+                        break;
+                    case "Requires":
+                        requires = requires.concat(UseFile.insertValues(value))
+                        break;
+                    case "Imports":
+                        imports = imports.concat(UseFile.insertValues(value));
+                        break;
+                    default:
+                        console.log("WARNING: Unrecognized use-file token: '" + token + "'");
+                        break;
+                }
+            }
+            result = new UseFile(name, description, sourcePath, requires, imports);
+        }
+        return result;
+    }
+    private static insertValues(values: string): string[] {
+        var result: string[] = [];
+        if (values.indexOf(",") > -1) {
+            result = values.split(",").map((value: string) => {
+                return value.trim();
+            });
+        } else {
+            result.push(values.trim());
+        }
+        return result;
     }
 }
